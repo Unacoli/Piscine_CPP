e823a425a95ce2c74513720b06e92d052dc629b7
ARGOUSE Nao <nargouse@student.42.fr>
2022-10-10 02:38:09 +0200

    finished ex02 of cpp04

---
+++ CPP_04/ex03/ICharacter.hpp
@@ -0,0 +1,18 @@
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+# include <iostream>
+# include <string>
+# include "AMateria.hpp"
+
+class   AMateria;
+
+class ICharacter
+{
+private:
+    /* data */
+public:
+    ICharacter(/* args */);
+    ~ICharacter();
+};
+
+#endif
